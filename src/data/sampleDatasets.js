18b50d13e413a8615cb81bfe9490487c14c9192e
Dylan Duke <laytonmaes@gmail.com>
2022-04-20 14:54:19 -0600

    Set up initial files

---
+++ src/data/sampleDatasets.js
@@ -0,0 +1,30 @@
+
+
+const bookingsExample = [
+    {"id":"5fwrgu4i7k55hl6sz","userID":9,"date":"2022/04/22","roomNumber":1},
+    {"id":"5fwrgu4i7k55hl6t5","userID":43,"date":"2022/01/24","roomNumber":2},
+    {"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2022/01/10","roomNumber":3},
+    {"id":"5fwrgu4i7k55hl6t7","userID":20,"date":"2022/02/16","roomNumber":4},
+    {"id":"5fwrgu4i7k55hl6t8","userID":1,"date":"2022/02/05","roomNumber":5},
+    {"id":"5fwrgu4i7k55hl6t9","userID":38,"date":"2022/02/14","roomNumber":6}
+    {"id":"5fwrgu4i7k55hl6t7","userID":20,"date":"2022/02/16","roomNumber":2}
+]
+
+const customersExample = [
+    {"id":1,"name":"Leatha Ullrich"},
+    {"id":2,"name":"Rocio Schuster"},
+    {"id":3,"name":"Kelvin Schiller"},
+    {"id":4,"name":"Kennedi Emard"},
+    {"id":5,"name":"Rhiannon Little"},
+    {"id":6,"name":"Fleta Schuppe"},
+    {"id":7,"name":"Dell Rath"},
+]
+
+const roomsExample = [
+    {"number":1,"roomType":"residential suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":358.4},
+    {"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":477.38},
+    {"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14},
+    {"number":4,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":1,"costPerNight":429.44},
+    {"number":5,"roomType":"single room","bidet":true,"bedSize":"queen","numBeds":2,"costPerNight":340.17},
+    {"number":6,"roomType":"junior suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":397.02},
+    {"number":7,"roomType":"single room","bidet":false,"bedSize":"queen","numBeds":2,"costPerNight":231.46}]
--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,8 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//------------------- api calls -------------------//
+import apiCalls from './apiCalls';
 
+// ------------------- main code -------------------//
 console.log('This is the JavaScript entry file - your code begins here.');
